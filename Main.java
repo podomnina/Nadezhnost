8e09baddedcad9f24d60afcfd6f9feddeef215a2
podomnina <domninal17@yandex.ru>
2016-12-30 14:05:08 +0300

    Update Main.java
    added protect

--- Main.java
+++ Main.java
@@ -19,24 +19,36 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
 import sample.entities.Vertex;
+import sample.entities.VertexLine;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main extends Application {
 
     private int counter = 0;
+    private int lineCounter = 0;
     private double radius = 20.0;
     private List<Vertex> vertices = new ArrayList<>();
+    private Set<VertexLine> lines = new HashSet<>();
+    private Button createVertex = new Button("Добавить вершину");
+    private Button createLine = new Button("Добавить связь");
+    private Button replaceVertex = new Button("Переместить вершины");
+    private Button calculate = new Button("Посчитать!");
+    private StackPane firstVertex = null;
+    private StackPane secondVertex = null;
 
     private void init(Stage primaryStage) {
         Group root = new Group();
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, getSampleWidth(),getSampleHeight()));
         ToolBar toolbar = new ToolBar();
-        toolbar.getItems().add(new Button("Добавить вершину"));
-        toolbar.getItems().add(new Button("Options"));
-        toolbar.getItems().add(new Button("Help"));
+        toolbar.getItems().add(createVertex);
+        toolbar.getItems().add(createLine);
+        toolbar.getItems().add(replaceVertex);
+        toolbar.getItems().add(calculate);
 
         toolbar.getItems().get(2).setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
@@ -57,6 +69,14 @@ public class Main extends Application {
             public void handle(MouseEvent event) {
                 System.out.println("X = " + event.getSceneX() + " Y = " + event.getSceneY());
                 addVertex(root, event.getSceneX(), event.getSceneY(), (Button) toolbar.getItems().get(1));
+            }
+        });
+
+        calculate.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Controller controller = new Controller(lines);
+                controller.makeScheme();
             }
         });
 
@@ -84,38 +104,65 @@ public class Main extends Application {
         layout.setLayoutX(x-radius);
         layout.setLayoutY(y-radius);
         root.getChildren().add(layout);
-        vertices.add(new Vertex(counter++,x-radius,y-radius, circle));
-        layout.setOnMouseDragged(new EventHandler<MouseEvent>() {
+        vertices.add(new Vertex(counter++,x-radius,y-radius, layout));
+
+        //TODO replace vertecies
+
+        replaceVertex.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                layout.setLayoutX(event.getSceneX()-radius);
-                layout.setLayoutY(event.getSceneY()-radius);
-                vertices.forEach(vertex -> {
-                    if (layout.getChildren().get(0).equals(vertex.circle)){
-                        vertex.x=event.getSceneX();
-                        vertex.y=event.getSceneY();
+                layout.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        layout.setLayoutX(event.getSceneX()-radius);
+                        layout.setLayoutY(event.getSceneY()-radius);
+                        vertices.forEach(vertex -> {
+                            if (layout.getChildren().get(0).equals(vertex.layout)){
+                                vertex.x=event.getSceneX();
+                                vertex.y=event.getSceneY();
+                            }
+                        });
                     }
                 });
             }
         });
-        circle.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+        layout.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                Line line = new Line();
-                line.setStartX(circle.getCenterX());
-                line.setStartY(circle.getCenterY());
-                line.setEndX(circle.getCenterX());
-                line.setEndY(circle.getCenterY());
-                root.getChildren().add(line);
+                if (lineCounter == 0){
+                    firstVertex = layout;
+                    lineCounter++;
+                    circle.setFill(Color.LIME);
+                } else if (lineCounter == 1) {
+                    secondVertex = layout;
+                    lineCounter = 0;
+                    circle.setFill(Color.SEAGREEN);
+                    VertexLine vertexLine = new VertexLine(searchVertex(firstVertex), searchVertex(secondVertex));
+                    if (!isContainVertexLine(vertexLine, lines)) {
+                        lines.add(vertexLine);
 
+                        Line line = new Line();
+                        line.setStartX(firstVertex.getLayoutX() + radius);
+                        line.setStartY(firstVertex.getLayoutY() + radius);
+                        line.setEndX(secondVertex.getLayoutX() + radius);
+                        line.setEndY(secondVertex.getLayoutY() + radius);
 
-                circle.setOnMouseDragged(new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent event) {
-                        line.setEndX(event.getSceneX());
-                        line.setEndY(event.getSceneY());
+                        root.getChildren().add(line);
                     }
-                });
+                    vertices.forEach(vertex -> {
+                        vertex.circle.setFill(Color.WHITE);
+                        vertex.layout.toFront();
+                    });
+                    firstVertex = null;
+                    secondVertex = null;
+                }
+            }
+        });
+        layout.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                layout.toFront();
             }
         });
     }
@@ -126,4 +173,18 @@ public class Main extends Application {
         primaryStage.show();
     }
     public static void main(String[] args) { launch(args); }
+
+    private Vertex searchVertex(StackPane layout){
+        return vertices.stream().filter(vertex -> vertex.layout.equals(layout)).findFirst().get();
+    }
+
+    private boolean isContainVertexLine(VertexLine vertexLine, Set<VertexLine> lines){
+        for (VertexLine line:lines) {
+            if (line.first.equals(vertexLine.first) && line.second.equals(vertexLine.second)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
